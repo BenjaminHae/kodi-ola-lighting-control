4a9845641dd0f3067411fe04b4e3fcfa3e2c386a
Benjamin Häublein <benjaminh@benjamin-VAIO.lan.benjaminh.de>
2015-08-31 16:20:15 +0200

    events+dispatcher

--- service.xbmc.dmx/service.py
+++ service.xbmc.dmx/service.py
@@ -1,6 +1,57 @@
 import time
 import xbmc
- 
+import dmx
+
+class Player(xbmc.Player):
+    def __init__(self):
+        super(Player, self).__init__()
+
+    def onPlayBackStarted(self):
+        # wait for playback to be really available
+        for i in xrange(1, 40):
+            if not (self.isPlayingAudio() or self.isPlayingVideo()):
+                if i == 40:
+                    return
+                else:
+                    xbmc.sleep(250)
+        self.onPlayBackStartedEx()#do dmx things here
+
+        def onPlayBackStopped(self):
+            pass
+        def onPlayBackEnded(self):
+            pass
+        def onPlayBackPaused(self):
+            pass
+        def onPlayBackResumed(self):
+            pass
+
+class Monitor(xbmc.Monitor):
+    # Energy Savings mode
+    def onDPMSActivated(self):
+        pass
+    def onDPMSDeactivated(self):
+        pass
+    def onScreensaverActivated(self):
+        pass
+    def onScreensaverDeactivated(self):
+        pass
+
+class Dispatcher:
+    pass
+
+class Light:
+    states={'black':0,'dark':1,'night':2,'medium':3,'bright':4,'full':5}
+    dmxstate=[[0,0,0,255],[75,75,75,255],[120,120,120,255],[150,150,150,255],[200,200,200,255],[255,255,255,255]]
+    currentState=0
+    def setStateByName(self, state):
+        pass
+    def setState(self, state):
+        pass
+    def more(self):
+        pass
+    def less(self):
+        pass
+
 if __name__ == '__main__':
     monitor = xbmc.Monitor()
     while not monitor.abortRequested():
