eea51c8f5711d7bbf6cd50665faae3ad030956de
Benjamin H盲ublein <BenjaminHae@users.noreply.github.com>
2015-09-03 18:48:08 +0200

    maybe fixed thread problems

--- service.xbmc.dmx/resources/lib/dmx.py
+++ service.xbmc.dmx/resources/lib/dmx.py
@@ -30,11 +30,14 @@ class DMXControl:
         self.cDMX = [0.0]*channels
         self.dDMX = [0.0]*channels
         self.aTime = [0.0]*channels
-        self.wrapper = ClientWrapper()
-        self.wrapper.AddEvent(self.TICK_INTERVAL, WrapperCallback)
-        self.thread = threading.Thread(target=self.wrapper.Run)
+        self.thread = threading.Thread(target=self.wrapperThread)
         self.thread.start()
         #self.wrapper.Run()
+        
+    def wrapperThread(self):
+        self.wrapper = ClientWrapper()
+        self.wrapper.AddEvent(self.TICK_INTERVAL, WrapperCallback)
+        self.wrapper.Run
     
     def stop(self):
         self.wrapper.Stop()
